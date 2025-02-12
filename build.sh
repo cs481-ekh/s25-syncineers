05714134e5d73f17af46e3d95210a962d689e304
Tyler Pierce <122319482+tylerpierce683@users.noreply.github.com>
2025-02-12 13:35:51 -0700

    Closes #43 (#44)

--- build.sh
+++ build.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 set -e # Exit on error
 
-if find lib test -name '*.dart' | grep .; then
-    echo "Flutter files found"
-    echo "Installing flutter dependencies..."
-    flutter config --enable-windows-desktop
-    flutter pub get
+echo "Installing flutter dependencies..."
+flutter config --enable-windows-desktop
+flutter pub get
 
-    echo "Building Flutter Windows App..."
-    if flutter build windows; then
-        echo "Build Complete"
-    else    
-        echo "Build Failed" && exit 127
-    fi
-else
-    echo "No Flutter files found, skipping build."
+echo "Building Flutter Windows App..."
+if flutter build windows; then
+    echo "Build Complete"
+else    
+    echo "Build Failed" && exit 127
 fi
