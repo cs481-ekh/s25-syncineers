c900394134162dd20abdec5c13958f986aee5166
Tyler Pierce <122319482+tylerpierce683@users.noreply.github.com>
2025-02-13 09:59:07 -0700

    Closes #46 (#47)

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Running all flutter tests designated in test/ directory..."
-if flutter test; then
-    echo "Tests complete."
-else 
-    echo "Tests failed." && exit 127
-fi
