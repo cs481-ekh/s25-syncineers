f28c995ef2dec5fcd87da90ec1a79a9f77e1c8c2
Tyler Pierce <tylerpierce683@u.boisestate.edu>
2025-02-18 09:44:43 -0700

    Changed build CI to build web app

--- build.ps1
+++ build.ps1
@@ -6,9 +6,9 @@ Write-Output "Installing Flutter dependencies..."
 flutter config --enable-windows-desktop
 flutter pub get
 
-Write-Output "Building Flutter Windows App..."
+Write-Output "Building Flutter Web App..."
 try {
-    flutter build windows
+    flutter build web
     Write-Output "Build Complete"
 } catch {
     Write-Output "Build Failed"
