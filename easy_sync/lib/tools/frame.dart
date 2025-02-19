e815a88e620629759680c34e90bda87a02921fa3
Aidan Flinn <aidanflinn@u.boisestate.edu>
2025-02-18 20:40:43 -0700

    adding frame widget

---
+++ easy_sync/lib/tools/frame.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class Frame extends StatefulWidget {
+  final Widget child;
+  final String title;
+
+  const Frame({super.key, required this.title, required this.child});
+
+  @override
+  State<Frame> createState() => _FrameState();
+}
+
+class _FrameState extends State<Frame> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        actions: [
+          IconButton(
+            onPressed: () => Navigator.of(context).pushReplacementNamed('/'),
+            icon: const Icon(Icons.home),
+          ),
+        ],
+      ),
+      //used to display add button throughout all pages
+      body: widget.child,   
+   );
+  }
+}
