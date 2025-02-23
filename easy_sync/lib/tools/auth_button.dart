81d699fac27b8653c92f4288329da66f6b80214f
Aidan Flinn <aidanflinn@u.boisestate.edu>
2025-02-23 14:25:41 -0700

    testing google calendar api, getting calendar sets from user

--- easy_sync/lib/tools/auth_button.dart
+++ easy_sync/lib/tools/auth_button.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:googleapis/calendar/v3.dart' as calendar;
+import 'package:googleapis_auth/googleapis_auth.dart' as auth;
+import 'package:http/http.dart' as http;
+import 'package:http/src/client.dart';
+
 
 class GoogleSignInButton extends StatefulWidget {
   const GoogleSignInButton({super.key});
@@ -54,6 +59,10 @@ class _GoogleSignInButtonState extends State<GoogleSignInButton> {
             onPressed: _handleSignOut,
             child: Text('Sign Out'),
           ),
+          ElevatedButton(
+            onPressed: getCalendarList,
+            child: Text('Get Calendar List'),
+          ),
         ],
       );
     } else {
@@ -66,4 +75,39 @@ class _GoogleSignInButtonState extends State<GoogleSignInButton> {
       );
     }
   }
+
+  Future<void> getCalendarList() async {
+    if (_currentUser == null) {
+      print("User not signed in");
+      return;
+    }
+    
+    try {
+      final GoogleSignInAuthentication googleAuth = await _currentUser!.authentication;
+
+      final auth.AccessCredentials credentials = auth.AccessCredentials(
+        auth.AccessToken("Bearer", googleAuth.accessToken!, DateTime.now().toUtc()),
+        googleAuth.idToken,
+        ['https://www.googleapis.com/auth/calendar'],
+      );
+
+      final auth.AuthClient client = auth.authenticatedClient(
+        http.Client(),
+        credentials,
+      );
+
+      final calendar.CalendarList calendarList = await calendar.CalendarApi(client).calendarList.list();
+
+      print('Calendar List:'); 
+      for (var item in calendarList.items ?? []) {
+        print('${item.summary} (${item.id})');
+      }
+      client.close();
+    } catch (e) {
+      print('Failed to get calendar list: $e');
+
+    
+    }
+  }
+  
 }
