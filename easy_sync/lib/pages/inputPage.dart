1de3d1968a18ec96bd161965d8ee0f120494a3db
Tyler Pierce <tylerpierce683@u.boisestate.edu>
2025-02-24 23:57:47 -0700

    Added file uploading

--- easy_sync/lib/pages/inputPage.dart
+++ easy_sync/lib/pages/inputPage.dart
@@ -2,13 +2,23 @@ import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'dart:io';
 
-class InputPage extends StatelessWidget {
+class InputPage extends StatefulWidget {
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  String? filePath;
 
   void getFile() async {
-    FilePickerResult? result = await FilePicker.platform.pickFiles(type: FileType.custom, allowedExtensions: ['.csv', '.xlsx']);
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      type: FileType.custom, 
+      allowedExtensions: ['csv', 'xls', 'xlsx']
+    );
 
     if (result != null) {
-      File file = File(result.files.first.path!);
+      setState(() {
+        filePath = result.files.single.path!;
+      });
     }
   }
 
@@ -18,7 +28,18 @@ class InputPage extends StatelessWidget {
       children: [
         IconButton(
           onPressed: getFile, 
-          icon: const Icon(Icons.file_open))
+          icon: const Icon(Icons.file_open, color: Colors.blue, size: 100.0),
+        ),
+        const SizedBox(height: 20,),
+        if (filePath != null)
+          Text(
+            'Selected file:\n${filePath!}',
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              fontSize: 16,
+              color: Colors.blue
+            ),
+          )
       ],
     );
   }
