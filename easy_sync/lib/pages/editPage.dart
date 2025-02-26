0c7215e89a3577711521bcd0cf037484bad8b3a6
CodeTeal422 <=>
2025-02-25 17:08:43 -0700

    list experimentation

--- easy_sync/lib/pages/editPage.dart
+++ easy_sync/lib/pages/editPage.dart
@@ -11,30 +11,33 @@ class _EditPageState extends State<EditPage> {
   @override
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
+    var questionToAnswer = 'Some question statement';
+    var list = [
+      "Selectable 1",
+      "Selectable 2",
+      "Selectable 3",
+      "Selectable 4",
+      "Selectable 5",
+    ];
+    var list2 = [
+      "Answer1",
+      "Answer2",
+      "Answer1",
+      "Answer4",
+    ];
 
+    return QuestionWidget(theme, questionToAnswer, list2);
+  }
+
+  Center QuestionWidget(
+      ThemeData theme, String questionToAnswer, List<String> listOfAnswers) {
     return Center(
       child: Card(
-        color: theme.colorScheme.primary,
         child: Column(
           children: [
-            Text("Some question statement"),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("Answer1"),
-                Text("Answer2"),
-                Text("Answer1"),
-                Text("Answer4"),
-              ],
-            ),
+            Text(questionToAnswer),
             Column(
-              children: [
-                Text("Selectable 1"),
-                Text("Selectable 2"),
-                Text("Selectable 3"),
-                Text("Selectable 4"),
-                Text("Selectable 5"),
-              ],
+              children: [for (var element in listOfAnswers) Text(element)],
             )
           ],
         ),
