6fabdb8aec250364836dd7ec5cdb7dee3fe4eda3
Ethan Barnes <ethanbarnes743@u.boisestate.edu>
2025-02-27 22:18:58 -0700

    technically somewhat complete

--- easy_sync/lib/pages/editPage.dart
+++ easy_sync/lib/pages/editPage.dart
@@ -19,28 +19,66 @@ class _EditPageState extends State<EditPage> {
       "Selectable 4",
       "Selectable 5",
     ];
-    var list2 = [
-      "Answer1",
-      "Answer2",
-      "Answer1",
-      "Answer4",
-    ];
 
-    return QuestionWidget(theme, questionToAnswer, list2);
+    return QuestionWidget(questionToAnswer: questionToAnswer, selectableAnswers: list);
   }
 
-  Center QuestionWidget(
-      ThemeData theme, String questionToAnswer, List<String> listOfAnswers) {
-    return Center(
-      child: Card(
-        child: Column(
-          children: [
-            Text(questionToAnswer),
-            Column(
-              children: [for (var element in listOfAnswers) Text(element)],
-            )
-          ],
-        ),
+}
+
+class QuestionWidget extends StatefulWidget {
+  const QuestionWidget({
+    super.key,
+    required this.questionToAnswer,
+    required this.selectableAnswers,
+  });
+
+  final String questionToAnswer;
+  final List<String> selectableAnswers;
+
+  @override
+  _QuestionWidgetState createState() => _QuestionWidgetState();
+}
+
+class _QuestionWidgetState extends State<QuestionWidget> {
+  List<String> selectedAnswers = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        children: [
+          Text(widget.questionToAnswer),
+          Expanded(
+            child: ListView.builder(
+                itemCount: selectedAnswers.length,
+                itemBuilder: (context, index) {
+                  ///TODO put varables here, maybe
+            
+                  return ListTile(
+                      title: Text(selectedAnswers[index]),
+                      onTap: () async {
+                        setState(() {
+                          selectedAnswers.removeAt(index);
+                        });
+                      });
+                }),
+          ),
+          Expanded(
+            child: ListView.builder(
+                itemCount: widget.selectableAnswers.length,
+                itemBuilder: (context, index) {
+                  ///TODO put varables here, maybe
+            
+                  return ListTile(
+                      title: Text(widget.selectableAnswers[index]),
+                      onTap: () async {
+                        setState(() {
+                          selectedAnswers.add(widget.selectableAnswers[index]);
+                        });
+                      });
+                }),
+          ),
+        ],
       ),
     );
   }
