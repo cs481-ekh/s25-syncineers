f31ea8973934a2bf5db2cb52595b14181cb0f34a
Ethan Barnes <ethanbarnes743@u.boisestate.edu>
2025-03-01 00:54:38 -0700

    random spaghetti code

--- easy_sync/lib/pages/editPage.dart
+++ easy_sync/lib/pages/editPage.dart
@@ -10,19 +10,26 @@ class EditPage extends StatefulWidget {
 class _EditPageState extends State<EditPage> {
   @override
   Widget build(BuildContext context) {
-    var theme = Theme.of(context);
-    var questionToAnswer = 'Some question statement';
+    Theme.of(context);
+    var questionToAnswer = 'What is the title of the events';
     var list = [
-      "Selectable 1",
-      "Selectable 2",
-      "Selectable 3",
-      "Selectable 4",
-      "Selectable 5",
+      questionAnswer("Subject", "GEOG"),
+      questionAnswer("Descr", "Introduction to Geography"),
+      questionAnswer("Course ID", "102477"),
+      questionAnswer("Room", "BLDR205"),
+      questionAnswer("Meeting Dates", "03/10/2025-05/02/2025"),
     ];
 
-    return QuestionWidget(questionToAnswer: questionToAnswer, selectableAnswers: list);
+    return QuestionWidget(
+        questionToAnswer: questionToAnswer, selectableAnswers: list);
   }
+}
 
+class questionAnswer {
+  late String title;
+  late String example;
+
+  questionAnswer(this.title, this.example);
 }
 
 class QuestionWidget extends StatefulWidget {
@@ -33,51 +40,69 @@ class QuestionWidget extends StatefulWidget {
   });
 
   final String questionToAnswer;
-  final List<String> selectableAnswers;
+  final List<questionAnswer> selectableAnswers;
 
   @override
   _QuestionWidgetState createState() => _QuestionWidgetState();
 }
 
 class _QuestionWidgetState extends State<QuestionWidget> {
-  List<String> selectedAnswers = [];
+  List<questionAnswer> selectedAnswers = [];
+  // TODO there probably is a better way of doing this.
+  Color cardShade = const Color.fromARGB(1, 126, 126, 126);
 
   @override
   Widget build(BuildContext context) {
+    String example = "";
+    for (var answer in selectedAnswers) {
+      example += "${answer.example} ";
+    }
+
     return Card(
       child: Column(
         children: [
           Text(widget.questionToAnswer),
+          Text("Example output: $example"),
           Expanded(
-            child: ListView.builder(
-                itemCount: selectedAnswers.length,
-                itemBuilder: (context, index) {
-                  ///TODO put varables here, maybe
-            
-                  return ListTile(
-                      title: Text(selectedAnswers[index]),
-                      onTap: () async {
-                        setState(() {
-                          selectedAnswers.removeAt(index);
+            child: Card(
+              color: cardShade,
+              child: ListView.builder(
+                  itemCount: selectedAnswers.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                        title: Text(selectedAnswers[index].title),
+                        onTap: () async {
+                          setState(() {
+                            selectedAnswers.removeAt(index);
+                          });
                         });
-                      });
-                }),
+                  }),
+            ),
           ),
           Expanded(
-            child: ListView.builder(
-                itemCount: widget.selectableAnswers.length,
-                itemBuilder: (context, index) {
-                  ///TODO put varables here, maybe
-            
-                  return ListTile(
-                      title: Text(widget.selectableAnswers[index]),
-                      onTap: () async {
-                        setState(() {
-                          selectedAnswers.add(widget.selectableAnswers[index]);
+            flex: 2,
+            child: Card(
+              color: cardShade,
+              child: ListView.builder(
+                  itemCount: widget.selectableAnswers.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                        title: Text(
+                            "${widget.selectableAnswers[index].title} | ${widget.selectableAnswers[index].example}"),
+                        onTap: () async {
+                          setState(() {
+                            selectedAnswers
+                                .add(widget.selectableAnswers[index]);
+                          });
                         });
-                      });
-                }),
+                  }),
+            ),
           ),
+          TextButton(
+              onPressed: () {
+                /**TODO send information off to context when pressed */
+              },
+              child: const Text("Answer question")),
         ],
       ),
     );
